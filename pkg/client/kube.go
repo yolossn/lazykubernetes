e2e135b751c8a1b50e0f1a289ccd372d6d4f8c5f
Nityananda Gohain <nityanandagohain@gmail.com>
2020-09-12 13:47:17 +0530

    list pods added in k8s client (#10)
    * list pods added in k8s client
    
    * debug statement removed

--- pkg/client/kube.go
+++ pkg/client/kube.go
@@ -2,6 +2,7 @@ package client
 
 import (
 	"context"
+	"fmt"
 	"path/filepath"
 	"time"
 
@@ -47,27 +48,52 @@ func (k *K8s) ListNamespace() ([]NamespaceInfo, error) {
 	ctx := context.TODO()
 	opts := v1.ListOptions{}
 	list, _ := k.client.CoreV1().Namespaces().List(ctx, opts)
-	// fmt.Println(list, err)
 	ns := []NamespaceInfo{}
 	for _, item := range list.Items {
 		n := NamespaceInfo{
-			Name:   item.ObjectMeta.Name,
-			Status: string(item.Status.Phase),
-			CreatedAt:    item.ObjectMeta.CreationTimestamp.Time,
+			Name:      item.ObjectMeta.Name,
+			Status:    string(item.Status.Phase),
+			CreatedAt: item.ObjectMeta.CreationTimestamp.Time,
 		}
 		ns = append(ns, n)
 	}
 	return ns, nil
 }
 
-// TODO: Create Struct for output similar to namespace
-func (k *K8s) ListPods(namespace string) ([]string, error) {
+type PodInfo struct {
+	Name      string
+	Status    string
+	Ready     string
+	Restarts  int32
+	CreatedAt time.Time
+}
+
+func (k *K8s) ListPods(namespace string) ([]PodInfo, error) {
 	ctx := context.TODO()
 	opts := v1.ListOptions{}
 	pods, _ := k.client.CoreV1().Pods(namespace).List(ctx, opts)
-	podList := []string{}
+	podList := []PodInfo{}
 	for _, pod := range pods.Items {
-		podList = append(podList, pod.Name)
+		restarts := int32(0)
+		ready := 0
+		totalContianers := len(pod.Status.ContainerStatuses)
+		for _, container := range pod.Status.ContainerStatuses {
+			if container.RestartCount > restarts {
+				restarts = container.RestartCount
+			}
+			if container.State.Running != nil {
+				ready++
+			}
+		}
+
+		p := PodInfo{
+			Name:      pod.Name,
+			Status:    string(pod.Status.Phase),
+			Restarts:  restarts,
+			Ready:     fmt.Sprintf("%v/%v", ready, totalContianers),
+			CreatedAt: pod.ObjectMeta.CreationTimestamp.Time,
+		}
+		podList = append(podList, p)
 	}
 	return podList, nil
 }
