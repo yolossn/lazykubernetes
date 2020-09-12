32c43e977c5aff55415ee6e9208098802e86cf4d
yolossn <nssvlr@gmail.com>
2020-09-12 10:31:16 +0530

    set basic keybindings

---
+++ pkg/gui/info.go
@@ -0,0 +1,17 @@
+package gui
+
+import "github.com/jesseduffield/gocui"
+
+
+func (gui *Gui) getInfoView() *gocui.View {
+	v, _ := gui.g.View("info")
+	return v
+}
+
+func (gui *Gui) onInfoClick(g *gocui.Gui, v *gocui.View) error {
+	if _, err := g.SetCurrentView(v.Name()); err != nil {
+		return err
+	}
+	return nil
+}
+
